5910f8f66c10c6792f9a23739d1e214e502dca14
grcavalier92 <982297627@qq.com>
2019-12-20 17:33:14 +0800

    itoken-admin

---
+++ itoken-admin/src/test/java/com/lzz/admin/ItokenAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzz.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItokenAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
